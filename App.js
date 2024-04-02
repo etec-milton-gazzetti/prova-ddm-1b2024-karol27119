d808d3c39dcd7c12e36e395743469be30756ad49
karol27119 <116905552+karol27119@users.noreply.github.com>
2024-04-02 12:52:22 +0000

    firts commit

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View, ImageBackground, Image } from 'react-native';
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
 
-export default function App() {
+import fundo from "././assets/fundo.png"
+
+function HomeScreen({navigation}) {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ImageBackground source={fundo} resizeMode="cover" style={styles.image}>
+      <Text>Acessibilidade</Text>
+        </ImageBackground>
     </View>
   );
 }
 
+  function AboutScreen({navigation}) {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Tela de Sobre</Text>
+        <TouchableOpacity onPress={()=>navigation.navigate('Home')}>
+        <Text>Voltar</Text>
+      </TouchableOpacity>
+      </View>
+    );
+  }
+
+  const Stack = createNativeStackNavigator();
+    
+  export default function App() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="About" component={AboutScreen}/>
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+  },
+  image: {
+    flex: 1,
     justifyContent: 'center',
   },
 });
