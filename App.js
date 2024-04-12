22791975d749d43fac39306a5968b0afea03153a
karol27119 <116905552+karol27119@users.noreply.github.com>
2024-04-12 12:38:54 +0000

    [feat] final commit

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TouchableOpacity, View, ImageBackground, Image } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View, ImageBackground, Image,SafeAreaView, ScrollView} from 'react-native';
 import {createNativeStackNavigator} from "@react-navigation/native-stack";
 import { useFonts, BebasNeue_400Regular } from '@expo-google-fonts/bebas-neue';
 
@@ -8,6 +8,7 @@ import fundo from "././assets/fundo.png";
 import fundosobre from "././assets/telasobre.png";
 import foto from "././assets/eu.jpg";
 import fot from "././assets/pageone.png";
+import acessibilidade from "././assets/acessibilidade.jpg";
 
 function Home({navigation}) {
   
@@ -20,7 +21,7 @@ function Home({navigation}) {
       <Text style={styles.text}>O que é</Text>
       </TouchableOpacity>
       <Text></Text>
-      <TouchableOpacity style={styles.bot} onPress={()=>navigation.navigate('Sobre')}>
+      <TouchableOpacity style={styles.bot} onPress={()=>navigation.navigate('Pag2')}>
       <Text style={styles.text}>História</Text>
       </TouchableOpacity>
       <Text></Text>
@@ -51,6 +52,36 @@ function Pag1({navigation}) {
     </View>
   );
 }
+
+function Pag2({navigation}) {
+  return (
+<ImageBackground source={fundosobre} resizeMode="cover" style={styles.image}>
+<SafeAreaView style={styles.container}>
+      <ScrollView style={styles.scrollView}>
+      <Text style={styles.titule}>Como surgiu a Acessibilidade?</Text>
+      <Text></Text>
+      <Image source={acessibilidade} style={styles.img}/>
+      <Text></Text>
+      <Text style={styles.sube}>O surgimento da acessibilidade remonta ao século XX, 
+      impulsionado por necessidades pós-guerra e avanços legislativos. A Lei de Reabilitação 
+      de 1973 nos EUA foi um marco, proibindo discriminação e incentivando mudanças em espaços 
+      públicos. Esse movimento inicial inspirou esforços contínuos em todo o mundo para tornar
+      ambientes e recursos acessíveis a todas as pessoas.</Text>
+      <Text></Text>
+      <Text style={styles.tituln}>Vertentes da Acessibilidade</Text>
+      <Text></Text>
+      <Text style={styles.sube}>Acessibilidade Arquitetônica: Design de espaços físicos para inclusão de todas as pessoas.</Text>
+      <Text style={styles.sube}>Acessibilidade Digital: Adaptação de tecnologias digitais para torná-las acessíveis a pessoas com deficiência.</Text>     
+      <Text style={styles.sube}>Acessibilidade Digital: Adaptação de tecnologias digitais para torná-las acessíveis a pessoas com deficiência.</Text>
+      <Text></Text>
+      <TouchableOpacity style={styles.bot} onPress={()=>navigation.navigate('Home')}>
+    <Text style={styles.text}>Voltar</Text>
+    </TouchableOpacity>
+      </ScrollView>
+    </SafeAreaView>
+    </ImageBackground>
+  );
+};
 
   function Sobre({navigation}) {
     return (
@@ -84,6 +115,7 @@ function Pag1({navigation}) {
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="Sobre" component={Sobre}/>
         <Stack.Screen name="Pag1" component={Pag1}/>
+        <Stack.Screen name="Pag2" component={Pag2}/>
         </Stack.Navigator>
       </NavigationContainer>
     );
@@ -105,6 +137,13 @@ const styles = StyleSheet.create({
     justifyContent:"center",
     alignSelf:"center",
   },
+  img:{
+    width:320,
+    height:190,
+    borderRadius:20,
+    justifyContent:"center",
+    alignSelf:"center",
+  },
   titulo: {
     fontFamily: "BebasNeue_400Regular",
     justifyContent:"center",
@@ -116,6 +155,32 @@ const styles = StyleSheet.create({
   titul: {
     backgroundColor:"white",
     width:270,
+    height:40,
+    fontFamily: "BebasNeue_400Regular",
+    justifyContent:"center",
+    alignItems:"center",
+    textAlign:"center",
+    alignSelf:"center",
+    fontSize:35,
+  },
+  titule: {
+    marginTop:100,
+    backgroundColor:"white",
+    shadowOpacity:100,
+    width:370,
+    height:40,
+    fontFamily: "BebasNeue_400Regular",
+    justifyContent:"center",
+    alignItems:"center",
+    textAlign:"center",
+    alignSelf:"center",
+    fontSize:35,
+  },
+  tituln: {
+    marginTop:10,
+    backgroundColor:"white",
+    shadowOpacity:100,
+    width:370,
     height:40,
     fontFamily: "BebasNeue_400Regular",
     justifyContent:"center",
